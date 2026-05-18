f038522be79250b16ec86de496299fd5b810369b
Alex Maiorov <alexander.mmaiorov@gmail.com>
2026-05-18 15:34:20 +0200

    Revert "headless"
    This reverts commit 78dc91f48b082c8718fd40000c2189ad040a7e10.

--- openroad/scripts/01_floorplan.tcl
+++ openroad/scripts/01_floorplan.tcl
@@ -163,8 +163,8 @@ utl::report "###################################################################
 source scripts/power_grid.tcl
 
 # Save checkpoint
-# save_checkpoint 01_${proj_name}.floorplan
-report_image "01_${proj_name}.floorplan" false
+save_checkpoint 01_${proj_name}.floorplan
+# report_image "01_${proj_name}.floorplan" false
 
 utl::report "###############################################################################"
 utl::report "# Stage 01 complete: Checkpoint saved to ${save_dir}/01_${proj_name}.floorplan.zip"
